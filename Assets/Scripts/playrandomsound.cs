41278f63c0be7caec4558bdad51f95c7eb7c5ff7
Vedal987 <vedal876@gmail.com>
2017-01-10 15:51:47 +0000

    Organize

---
+++ Assets/Scripts/playrandomsound.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playrandomsound : MonoBehaviour {
+	
+	private AudioSource myaudio;
+	public AudioClip[] Sounds; 
+	public float delay;
+
+	private int ran;
+
+	void Start () {
+		myaudio = GetComponent<AudioSource>();
+		StartCoroutine (Play ());
+	}
+
+	IEnumerator Play()
+	{
+		yield return new WaitForSeconds (delay);
+		ran = Random.Range(1,Sounds.Length);
+		myaudio.clip = Sounds[ran];
+		myaudio.pitch = 0.9f + 0.1f *Random.value;
+		myaudio.PlayOneShot(myaudio.clip);
+
+		Sounds[ran] = Sounds[0];
+		Sounds[0] = myaudio.clip;
+	}
+
+}
